e0f053cb79d74ad32979322fd5b797fe2ec159f3
nchernovtest <zugzug90@yandex.ru>
2017-11-23 14:47:32 +0600

    TST-2 more more

--- super-duper.js
+++ super-duper.js
@@ -6,6 +6,7 @@ stack-profiles.label.stack-profiles.label.action.edit=Edit Stack Profile
 rvice.type.label.ec2=AWS EC2stack-profiles.se
 CloudFormationstack-profiles.service.type.label.cf=AWS action.edit=Edit Stack Profile
 rvice.type.label.ec2=AWS EC2stack-profiles.se
+
 CloudFormationstack-profiles.service.type.label.cf=AWS 
 stack-profiles.label.jira.groups=Permitted JIRA Groups
 stack-profil.pvicetype=Choose service type.label.description=Descr
