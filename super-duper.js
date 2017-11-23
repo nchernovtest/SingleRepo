9b449c2c8be289e0e2e36705a21678641b575160
nchernovtest <zugzug90@yandex.ru>
2017-11-23 14:31:41 +0600

    TST-2 removed redundant data

--- super-duper.js
+++ super-duper.js
@@ -4,8 +4,6 @@ stack-profiles.label.ac
 tion.add=Add Stack Profile
 stack-profiles.label.stack-profiles.label.action.edit=Edit Stack Profile
 rvice.type.label.ec2=AWS EC2stack-profiles.se
-CloudFormationstack-profiles.service.type.label.cf=AWS action.edit=Edit Stack Profile
-rvice.type.label.ec2=AWS EC2stack-profiles.se
 CloudFormationstack-profiles.service.type.label.cf=AWS 
 stack-profiles.label.jira.groups=Permitted JIRA Groups
 stack-profil.pvicetype=Choose service type.label.description=Descr
